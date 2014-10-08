c119ee1667b82f365e0f46c3f1eb6fb8c9869124
John Blischak <jdblischak@gmail.com>
2014-10-08 15:09:29 -0500

    Install RStudio.

--- setup.sh
+++ setup.sh
@@ -13,6 +13,14 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 apt-get update
 apt-get -y install r-base r-base-dev
 
+# RStudio
+# http://www.rstudio.com/products/rstudio/download-server/
+# https://support.rstudio.com/hc/en-us/articles/200552306-Getting-Started
+apt-get -y install gdebi-core libapparmor1
+cd /tmp
+wget http://download2.rstudio.org/rstudio-server-0.98.1062-amd64.deb
+gdebi -n rstudio-server-0.98.1062-amd64.deb
+
 # Python
 apt-get -y install python3 python3-pip
 pip3 install -r requirements.txt
