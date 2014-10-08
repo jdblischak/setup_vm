756e53e4cf717ba2a0beb4870644116f7693567c
John Blischak <jdblischak@gmail.com>
2014-10-08 10:39:58 -0500

    Install zip, unzip, emacs, and git.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 apt-get update
 apt-get -y upgrade
 
+# Utilities
+apt-get -y install zip unzip emacs git
+
 # R
 # http://cran.r-project.org/bin/linux/ubuntu/
 echo "deb http://cran.case.edu/bin/linux/ubuntu precise/" >> /etc/apt/sources.list
