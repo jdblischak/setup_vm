bb6c7341236c3754d8afc414e6d4cefd1c99da88
John Blischak <jdblischak@gmail.com>
2014-10-08 11:07:38 -0500

    Add key for accessing CRAN archives for Ubuntu.

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ apt-get -y install zip unzip emacs git
 # R
 # http://cran.r-project.org/bin/linux/ubuntu/
 echo "deb http://cran.case.edu/bin/linux/ubuntu precise/" >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 apt-get update
 apt-get -y install r-base r-base-dev
 
