86b57a39bcdad93290469048e70606a8513de0eb
John Blischak <jdblischak@gmail.com>
2014-10-10 07:49:13 -0500

    Add --global flag to git config setup.

--- configure_git.sh
+++ configure_git.sh
@@ -3,8 +3,8 @@
 # First argument is user.name.
 # Second argument is email address.
 
-git config user.name $1
-git config user.email $2
+git config --global user.name $1
+git config --global user.email $2
 # Use nano as editor, wrapping lines after 72 characters
 # http://git-scm.com/book/ch5-2.html#Commit-Guidelines
-git config core.editor "nano -r 72"
+git config --global core.editor "nano -r 72"
