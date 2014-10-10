6efe6d963c18f2706f4ebd9d89eb2fd502fc3659
John Blischak <jdblischak@gmail.com>
2014-10-10 07:52:23 -0500

    Add quotes around argument to set user.name.
    Otherwise only the first word gets added as the user.name.

--- configure_git.sh
+++ configure_git.sh
@@ -3,7 +3,7 @@
 # First argument is user.name.
 # Second argument is email address.
 
-git config --global user.name $1
+git config --global user.name "$1"
 git config --global user.email $2
 # Use nano as editor, wrapping lines after 72 characters
 # http://git-scm.com/book/ch5-2.html#Commit-Guidelines
